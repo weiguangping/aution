61d6eed9468bf4addffc3cb50ce6b100cbf073df
“tuimeo_weiguangping <weiguangping@tuimeo.cn>
2018-01-04 22:07:00 +0800

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
